2afe26fc7c16da4e6d93702c1f94a1b07794650b
Alessio Angeletti <alessioangeletti@libero.it>
2021-06-10 10:04:57 +0200

    Adding the "Comments" Features

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,15 +1,48 @@
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import { useParams } from 'react-router';
 
 import classes from './Comments.module.css';
 import NewCommentForm from './NewCommentForm';
+import useHttp from '../../hooks/use-http';
+import { getAllComments } from '../../lib/api';
+import LoadingSpinner from '../UI/LoadingSpinner';
+import CommentsList from './CommentsList';
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
+  const params = useParams();
+
+  const { quoteId } = params;
+
+  const { sendRequest, status, data: loadedComment } = useHttp(getAllComments);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [quoteId, sendRequest]);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
-  
+
+  const addedCommentHandler = useCallback(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  let comments;
+
+  if (status === 'pending')
+    comments = (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    );
+
+  if (status === 'completed' && loadedComment && loadedComment.length > 0)
+    comments = <CommentsList comments={loadedComment} />;
+
+  if (status === 'completed' && (!loadedComment || loadedComment.length === 0))
+    comments = <p className='centered'>No comments were added yet!</p>;
+
   return (
     <section className={classes.comments}>
       <h2>User Comments</h2>
@@ -18,8 +51,13 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && (
+        <NewCommentForm
+          quoteId={quoteId}
+          onAddedComment={addedCommentHandler}
+        />
+      )}
+      {comments}
     </section>
   );
 };
